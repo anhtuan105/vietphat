c9b957d4c2c2627c01765d497cadf1dd6939d93c
a-nguyen <a-nguyen@oz-vision.co.jp>
2017-08-02 20:44:05 +0900

    chuyen sang tieng nhat

---
+++ vietphat/protected/views/layouts/head_jp.php
@@ -0,0 +1,319 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta http-equiv="Content-Type" content="charset=utf-8" />
+<meta name="Description" content="CÔNG TY S&#7842;N XU&#7844;T VÀ TH&#431;&#416;NG M&#7840;I B&#7842;O AN - Nhà phân ph&#7889;i s&#7843;n ph&#7849;m M&#7923; Ch&#361; &#273;&#7897;c qu&#7873;n, mi&#7871;n, bún khô, ph&#7903; khô,... Liên h&#7879;: 0913.507.266
+">
+<meta name="KeyWords" content="ban my chu, ban my chu, phan phoi my chu, my bac giang, m&#7923; b&#7855;c giang, m&#7923; ch&#7911; b&#7855;c giang, &#273;&#7863;c s&#7843;n m&#7923; ch&#7911;, cung c&#7845;p m&#7923; ch&#361;, mì ch&#361; s&#7841;ch, phân ph&#7889;i m&#7923; ch&#361;, công ty bán m&#7923; ch&#361;, mi chu, mi&#7871;n so, mi&#7871;n s&#7841;ch, bánh &#273;a nem, ph&#7903; khô, mien sach, bun kho, pho kho, bán bún khô, cung c&#7845;p bún khô, cung c&#7845;p ph&#7903; khô, cung c&#7845;p mi&#7871;n, phân ph&#7889;i mi&#7871;n, phân ph&#7889;i bánh &#273;a nem cho các nhà hàng,">
+<meta name="generator" content="Công ty B&#7843;o An Food">
+
+<!-- css -->
+<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/style.css" />
+
+<script language="JavaScript">
+function clock() {
+if (!document.layers && !document.all) return;
+var digital = new Date();
+var hours = digital.getHours();
+var minutes = digital.getMinutes();
+var seconds = digital.getSeconds();
+if (hours == 0) hours = 12;
+if (minutes <= 9) minutes = "0" + minutes;
+if (seconds <= 9) seconds = "0" + seconds;
+dispTime = hours + ":" + minutes;
+if (document.layers) {
+document.layers.pendule.document.write(dispTime);
+document.layers.pendule.document.close();
+}
+
+else
+if (document.all)
+pendule.innerHTML = dispTime;
+
+setTimeout("clock()", 1000);
+}
+
+</script>
+
+
+<title>商品</title>
+<link rel="icon" href="http://dacsanmychu.com/images/icon.ico">
+
+<body cz-shortcut-listen="true">
+	<div id="headerbg">
+	  <div id="header-size">
+	    <div id="header-logo">
+	    	<div id="logo"> </div>
+	  		
+				<div id="text-mntop">
+<!--					<div id="ngaythang">-->
+<!--						<script language="javascript" type="text/javascript">-->
+<!--						<!-- Hide script from old browsers-->
+<!--					        dayName = new Array ("Ch&#7911; nh&#7853;t",'Th&#7913; 2','Th&#7913; 3','Th&#7913; 4','Th&#7913; 5','Th&#7913; 6','Th&#7913; 7')   -->
+<!--					        monName = new Array ("/1","/2","/3","/4","/5","/6","/7","/8","/9","/10","/11","/12")   -->
+<!--					        now = new Date-->
+<!--					        document.write(""-->
+<!--					        +dayName[now.getDay()]+ ", ng&agrave;y " +now.getDate()+ " " +monName[now.getMonth()]+ "/ " +now.getFullYear()+ " - <font style='color:#FF0000; font-weight:bold'>" +now.getHours()+ ":" +now.getMinutes()+ "</font> ")   -->
+<!--						//Ending hiding script from old browsers -->
+<!--						</script>-->
+<!--					</div>-->
+                    <div style="line-height: 15px;margin-left: -16px;font-size: 16px;"><strong>Email:</strong> <a href="mailto:tenmiensohuu@gmail.com" style="font-size: 15px;">tenmiensohuu@gmail.com</a></div>
+					<div id="sale">
+	  					<p>サポート 24/7： <strong>アップデート中</strong></p>
+	  					<p>携帯：<strong>アップデート中</strong></p>
+	  				</div>
+				</div>
+	    	</div> 
+	    <div id="nav-size">
+	    	<div id="nav">
+				<ul>
+				<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/ddlevelsmenu-base.css" />
+				<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/ddlevelsmenu-topbar.css" />
+				<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/ddlevelsmenu-sidebar.css" />
+				<script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl; ?>/js/ddlevelsmenu.js"></script>
+
+				<div id="ddtopmenubar" class="menu_horizontal">
+					<ul>
+						<li id="current"><?php echo CHtml::link('ホーム', array('homes/index')); ?></li>
+						<li><a href="#" rel="ddsubmenu959"><span>紹介</span></a></li>
+						<?php echo CHtml::link(CHtml::tag('span',array(), '商品'), array('categorys/index'),array('rel'=>'ddsubmenu1274')); ?>
+						<?php echo CHtml::link(CHtml::tag('span',array(), 'ニュース'), array('news/index')); ?>
+						<?php echo CHtml::link(CHtml::tag('span',array(), '野菜'), array('amthuc/index')); ?>
+						<?php echo CHtml::link(CHtml::tag('span',array(), '代理'), array('store/index')); ?>
+						<li><a href="#" rel="ddsubmenu955"><span>サポート</span></a></li>
+						<?php echo CHtml::link(CHtml::tag('span',array(), '情報'), array('baogia/index')); ?>
+						<?php echo CHtml::link(CHtml::tag('span',array(), '採用'), array('tuyendung/index')); ?>
+						<?php //echo CHtml::link(CHtml::tag('span',array(), '問い合わせ'), array('site/contact')); ?>
+					</ul>
+				</div>
+				<script type="text/javascript">
+				ddlevelsmenu.setup("ddtopmenubar", "topbar") //ddlevelsmenu.setup("mainmenuid", "topbar|sidebar")
+				</script>
+			</div>
+		</div>
+	</div>
+
+	<!-- downMenu Gioi Thieu-->
+	<ul id="ddsubmenu959" class="ddsubmenustyle" style="z-index: 2000; width: auto; height: auto; overflow: visible; opacity: 1; left: 0px; top: 0px; visibility: hidden;">
+		<?php
+			$intros = Introduces::model()->findAll();
+			// die(var_dump($categorys));
+			foreach ($intros as $key => $value) {
+				echo '<li>';
+				echo CHtml::link($value->introducecol,array('introduces/view','id'=>$value->id));
+				echo '</li>';
+			}
+		?>
+	</ul>
+
+	<!-- DropDown San Pham-->
+	<ul id="ddsubmenu1274" class="ddsubmenustyle" style="z-index: 2000; width: auto; height: auto; overflow: visible; opacity: 1; left: 0px; top: 0px; visibility: hidden;">
+		<?php
+			$categorys = Categorys::model()->findAll();
+			// die(var_dump($categorys));
+			foreach ($categorys as $key => $value) {
+				echo '<li>';
+				echo CHtml::link($value->name,array('categorys/view','id'=>$value->id));
+				echo '</li>';
+			}
+		?>
+	</ul>
+	<!-- Dropdown tu van,chinhsach-->
+	<ul id="ddsubmenu955" class="ddsubmenustyle" style="z-index: 2000; width: auto; height: auto; overflow: visible; opacity: 1; left: 0px; top: 0px; visibility: hidden;">
+		<?php
+			$rules = Chinhsach::model()->findAll(array("limit"=>5));
+			foreach ($rules as $key => $value) {
+				echo '<li>';
+				echo CHtml::link($value->name,array('Chinhsach/view','id'=>$value->id));
+				echo '</li>';
+			}
+		?>
+	</ul>
+
+	<!-- <div class="clear"></div> -->
+	<div id="product-content">
+		<div class="product-left-content">
+			<div id="product-hot-news">
+				<div id="navd-box">
+					<div id="tab-navd">ニュース</div>
+					<div id="navd-bg3">
+						<marquee behavior="scroll" direction="up" height="235" scrollamount="1" scrolldelay="20" border-bottom-left-radius="15px" onmouseover="this.stop()" onmouseout="this.start()">
+						<?php
+							$news = News::model()->findAll(array('order'=>'id DESC'));
+							if(isset($news)) {
+								foreach($news as $new) {
+									echo '<div id="box-tinr">';
+									echo '<div id="imag2">';
+										$imgLink = CHtml::image(Yii::app()->request->baseUrl.'/images/news/'.$new->imageLink, $new->title, array('width'=>'50', 'height'=>'42', 'border'=>'0'));
+										echo CHtml::link($imgLink, array('news/view', 'id'=>$new->id));
+									echo '</div>';
+									echo '<div id="textnews-2">';
+										if(strlen($new->title) > 100) {
+											$newCut = substr($new->title, 0, 100);
+											$newTitle = substr($newCut, 0, strrpos($newCut, ' '));
+										} else {
+											$newTitle = $new->title;
+										}
+										echo CHtml::link($newTitle, array('news/view', 'id'=>$new->id));
+									echo '</div>';
+									echo '</div>';
+								}
+							}
+						?>
+						</marquee>
+					</div>
+				</div>
+			</div>
+			<div id="navd-box">
+				<div id="tab-navd"> 商品 </div>
+    			<div id="navd-bg">
+    				<div id="menud">
+          				<ul>
+          					<?php
+          						foreach ($categorys as $key => $value) {
+          							echo '<li>';
+          							echo CHtml::link($value->name,array('categorys/view','id'=>$value->id));
+          							echo '</li>';
+          						}
+          					?>
+          				</ul>
+          			</div>
+          		</div>
+  			</div>
+			<!-- <div id="product-support"> -->
+			<div class="clear"></div>
+			<div id="navd-box">
+		    	<div id="tab-navd">オンライン　サポート</div>
+		        <div id="navd-bg2">
+		        	<div id="box-rig">
+		            	<div id="box-text-hot">アップデート中</div>
+		            	<div id="box-yahoo"><a class="PHstyle26" href="skype:anhtuan105"><img width="70" height="25" style="border: none;" src="<?php echo Yii::app()->request->baseUrl; ?>/images/sky.jpg"></a><div style="margin-top:-19px; margin-left:80px; color:#00CC33; text-decoration:underline">問い合わせ</div></div>
+					<div style="line-height: 15px;margin-left: -16px;font-size: 16px;"><strong>Email:</strong> <a href="mailto:tenmiensohuu@gmail.com" style="font-size: 15px;">tenmiensohuu@gmail.com</a></div>
+		                <div style="padding-top:15px;font-size: 14px;" id="box-thoigianhotro"><strong>サポート: 24/24</strong></div>
+		        	</div>
+		    	</div>
+		    </div>
+			<!-- </div> -->
+<!--			<div>-->
+<!--				<div id="fb-root"></div>-->
+<!--				<script>(function(d, s, id) {-->
+<!--				  var js, fjs = d.getElementsByTagName(s)[0];-->
+<!--				  if (d.getElementById(id)) return;-->
+<!--				  js = d.createElement(s); js.id = id;-->
+<!--				  js.src = "//connect.facebook.net/vi_VN/all.js#xfbml=1";-->
+<!--				  fjs.parentNode.insertBefore(js, fjs);-->
+<!--				}(document, 'script', 'facebook-jssdk'));</script>-->
+<!--				<div class="fb-like-box" data-href="http://www.facebook.com/FacebookDevelopers" data-width="239" data-height="280" data-colorscheme="light" data-show-faces="true" data-header="true" data-stream="false" data-show-border="true"></div>-->
+<!--			</div>-->
+		</div>
+		<div class="product-right-content">
+			<?php echo $content; ?>
+		</div>
+		<div class="clear"></div>
+	</div>
+
+	<!-- footer -->
+	<div id="footerbg">
+	<div id="footerblank">
+    	<div id="footerbox">
+<div id="footerbox2">
+    <div class="footerheading">
+    	<h4 class="footerheading">紹介</h4>
+    </div>
+    <div class="footertxt">
+    	<?php
+			$intros = Introduces::model()->findAll();
+			foreach ($intros as $key => $value) {
+				echo '<li>';
+				echo CHtml::link($value->introducecol,array('introduces/view','id'=>$value->id));
+				echo '</li>';
+			}
+		?>
+	</div>
+</div>	
+
+<div id="footerbox2">
+    <div class="footerheading">
+       	<h4 class="footerheading">商品</h4>
+    </div>
+    <div class="footertxt">
+
+    	<?php
+			$categorys = Categorys::model()->findAll(array("limit" => 5));
+			foreach ($categorys as $key => $value) {
+				echo '<li>';
+				echo CHtml::link($value->name,array('categorys/view','id'=>$value->id));
+				echo '</li>';
+			}
+		?>
+	</div>
+</div>	
+
+<div id="footerbox2">
+    <div class="footerheading">
+        <h4 class="footerheading">ポリシー</h4>
+    </div>
+    <div class="footertxt">
+    	<?php
+			foreach ($rules as $key => $value) {
+				echo '<li>';
+				echo CHtml::link($value->name,array('Chinhsach/view','id'=>$value->id));
+				echo '</li>';
+			}
+		?>
+	</div>
+</div>
+
+<div id="footerbox3">
+    <div class="footerheading">
+      	<h4 class="footerheading">集計</h4>
+    </div>
+    <div class="footertxt">
+<?php
+		$visited = Viewcounts::model()->visited();
+		echo '<a href="#" class="textmndichvu">オンライン: ' . $visited['total'] . '</a><br />';
+		echo '<a href="#" class="textmndichvu">カウント: ' . $visited['total'] . '</a><br />';
+?>
+	</div>
+</div>
+
+</div>
+</div>
+</div>
+
+<div id="footer-bg">
+	<div id="footerblank">
+        <div id="footerlinks">
+			<p>
+				<h4 style="line-height: 15px;">Email: <a href="mailto:tenmiensohuu@gmail.com">tenmiensohuu@gmail.com</a></h4>
+ 			</p>
+ 		</div>
+ 		<font color="#009933" size="1">
+      	<div id="coppyright">
+      		<h4>Design by<a href="#" target="_blank" class="coppyright">The World</a></h4>
+      		<h4>Copyright © 2014 - </h4>
+      	</div>
+  		</font></div><font color="#009933" size="1">
+		</font></div><font color="#009933" size="1">
+		</font></div>
+
+		<ul id="ddsubmenu959" class="ddsubmenustyle" style="z-index: 2000; width: auto; height: auto; overflow: visible; opacity: 1; left: 0px; top: 0px; visibility: hidden;">
+			<li><a href="#">概要</a></li>
+			<li><a href="#">紹介</a></li>
+			<li><a href="#">挨拶</a></li>
+			<span class="img_float" style="border-left: 0px solid #CCCCCC;"><img src="./mychu_files/menungang1.png"></span>
+		</ul>
+		<ul id="ddsubmenu1274" class="ddsubmenustyle" style="z-index: 2000; width: auto; height: auto; overflow: visible; opacity: 1; left: 0px; top: 0px; visibility: hidden;">
+			<span class="img_float" style="border-left: 0px solid #CCCCCC;"><img src="./mychu_files/menungang1.png"></span>
+		</ul>
+		<div id="tooltip" style="display: none;"><h3></h3>
+		<div class="body"></div>
+		<div class="url">
+		</div>
+	</div>
+	</div>
+	<!-- end footer -->
+
+	</div><!-- page -->
+
+</body>
+</html>
